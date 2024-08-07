5415d766376f44f7ebce723a7d6b25c90e078ca1
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2024-08-07 16:05:52 -0300

    análise dataset iris

--- python/analise_dados_fraude.py
+++ python/analise_dados_fraude.py
@@ -5,20 +5,21 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, accuracy_score
 
 
-dados = pd.read_csv('python/src/fraud_data.csv')
+
+dados = pd.read_csv('python/src/fraud_data1000.csv')
 
 
 # Selecionar características (features) e rótulos (labels)
-X = dados.drop('is_fraud', axis=1)  # Características
+x = dados.drop('is_fraud', axis=1)  # Características
 y = dados['is_fraud']  # Rótulos
 
 
 # Dividir dados em conjuntos de treinamento e teste
-X_treino, X_teste, y_treino, y_teste = train_test_split(X, y, test_size=0.2, random_state=42)
+X_treino, X_teste, y_treino, y_teste = train_test_split(x, y, test_size=0.2, random_state=42)
 
 
 # Treinar modelo de Floresta Aleatória
-modelo = RandomForestClassifier(n_estimators=100, random_state=42)
+modelo = RandomForestClassifier(n_estimators=1000, random_state=42)
 modelo.fit(X_treino, y_treino)
 
 
