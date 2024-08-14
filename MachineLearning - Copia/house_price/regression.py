b702bbd3c21f648d1686d8a56102fe20fa644537
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2024-08-13 21:01:16 -0300

    decision tree and linear regression - house_prices

---
+++ MachineLearning - Copia/house_price/regression.py
@@ -0,0 +1,68 @@
+# from sklearn.datasets import fetch_california_housing
+
+# housing = fetch_california_housing()
+
+from sklearn.datasets import fetch_openml
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+
+# Baixando o dataset de preços de casas do OpenML
+housing = fetch_openml(name="house_prices", as_frame=True)
+
+# Extraindo os dados e o alvo
+data = housing.data
+target = housing.target
+
+# Combinando os dados e o alvo em um único DataFrame
+df = pd.concat([data, target], axis=1)
+
+# Exibindo o DataFrame resultante
+print(df.head())
+print(df.info())
+print(df.describe())
+
+
+
+
+# Selecionando as melhores features com base na correlação
+features = ['OverallQual', 'GrLivArea', 'GarageCars']
+X = df[features]
+y = df['SalePrice']
+
+# Dividindo os dados em treino e teste
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+
+# Instanciando o modelo
+modelo = LinearRegression()
+
+# Treinando o modelo
+modelo.fit(X_train, y_train)
+
+
+
+
+# Fazendo previsões no conjunto de teste
+y_pred = modelo.predict(X_test)
+
+# Avaliando o modelo
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print(f"Erro Quadrático Médio: {mse:.2f}")
+print(f"R²: {r2:.2f}")
+
+# Criando um DataFrame para comparar valores reais e previstos
+resultado = pd.DataFrame({'Valor Real': y_test, 'Previsão': y_pred})
+
+# Exibindo as 10 primeiras comparações
+print(resultado.head(10))
+
+
